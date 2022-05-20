5a8f39e5be661b4bcb6cd666ee76019e239a7227
git@github.com:adrianapvent/git-it-done.git <adriana.e.pena@gmail.com>
2022-05-20 14:34:25 -0500

    able to type again

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const fs = require("fs");
+const uuid = require("./helpers/uuid");
 
 const path = require("path");
-
-const uuid = require("./helpers/uuid");
 
 const util = require("util");
 const readFromFile = util.promisify(fs.readFile);
@@ -18,7 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
@@ -28,16 +26,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-    console.log(`${req.method} request received to get notes`);
-
     readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-const writeToFile = (destination, note) => {
-    fs.writeFile(destination, JSON.stringify(note, null, 4), (err) =>
-        err ? console.log(err) : console.info("Successfully updated Notes"));
-};
 
+const writeToFile = (desination, note) => {
+    fs.writeFile(desination, JSON.stringify(note, null, 4), (err) =>
+    err ? console.log(err) : console.info("Successfully updated Notes"));
+};
 
 const append = (content, file) => {
     fs.readFile(file, "utf8", (err, data) => {
@@ -50,12 +46,6 @@ const append = (content, file) => {
         }
     });
 };
-
-app.route("/api/notes")
-    // Grab the notes list (this should be updated for every new note and deleted note.)
-    .get(function (req, res) {
-        res.json(database);
-    })
 
 app.post("/api/notes", (req, res) => {
     console.log(`${req.method} request received to add a note`);
@@ -79,30 +69,6 @@ app.post("/api/notes", (req, res) => {
         res.status(500).json("Error with note posting!");
     }
 });
-
-// app.delete("/api/notes/:id", function (req, res) {
-//     let jsonFilePath = path.join(__dirname, "/db/db.json");
-//     // request to delete note by id.
-//     for (let i = 0; i < database.length; i++) {
-
-//         if (database[i].id == req.params.id) {
-//             // Splice takes i position, and then deletes the 1 note.
-//             database.splice(i, 1);
-//             break;
-//         }
-//     }
-//     // Write the db.json file again.
-//     fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
-
-//         if (err) {
-//             return console.log(err);
-//         } else {
-//             console.log("Your note was deleted!");
-//         }
-//     });
-//     res.json(database);
-// });
-
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
